fc850f5de232456e342bc247e5ad2b87650b1c01
adeii <adeii@dr.com>
2018-12-31 19:30:02 +0100

    just testing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,8 +21,8 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
-# TARGET_CPU_VARIANT := cortex-a53 # Stock default.prop said generic
+# TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53 # Stock default.prop said generic
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
@@ -41,7 +41,6 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-# BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci slub_min_objects=12 unmovable_isolate1=2:192M,3:224M,4:256M androidboot.selinux=permissive buildvariant=eng
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_OFFSET := 0x00008000
@@ -51,12 +50,16 @@ TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/recovery/kernel
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb #No any image for London
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
+TW_USE_NEW_MINADBD := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE := true
+TARGET_CRYPTFS_HW_PATH := device/huawei/london/cryptfs_hw #work on Diego, maybe old for London
 
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
@@ -104,5 +107,5 @@ TW_INCLUDE_FUSE_EXFAT := true
 TW_INCLUDE_FUSE_NTFS := true
 
 # Treble
-BOARD_NEEDS_VENDORIMAGE_SYMLINK := false
-TARGET_COPY_OUT_VENDOR := vendor
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := false # not sure about it
+TARGET_COPY_OUT_VENDOR := vendor #or system/vendor in TWRP, also not sure, if above is TRUE
