25b8019b0cfe67470e31e7b0cd419dd278ee8c0e
adeii <adeii@dr.com>
2019-01-02 16:06:26 +0100

    Update full_london.mk

--- full_london.mk
+++ full_london.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/mini_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Charger
